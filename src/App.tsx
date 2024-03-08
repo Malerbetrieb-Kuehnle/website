b8c28e1222bbabc5c73619631d8198e0e6ce4988
Manuel Kühnle <ge36sej@mytum.de>
2024-03-09 00:51:37 +0100

    added sections and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import styles from "./style";
+
 import {
   Accordion,
   AccordionContent,
@@ -6,42 +8,182 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { Button } from "@/components/ui/button";
+
+// Import the images
+import fassade from "./assets/Fassade_Grabenstätt 1.png";
+import about_us from "./assets/25-Jahre.png";
+
+// Navbar
+import { Navbar } from "./components/ui/navbar";
+
+// Footer
+import { Footer } from "./components/ui/footer";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { services_innenbereich, services_aussenbereich } from './constants';
+
 function App() {
   return (
     <html>
-      <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title>Malerbetrieb</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <main>
-        <div>
+      <body>
+        {/* Navbar */}
+        <div className="w-full  overflow-hidden">
+          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Navbar />
+            </div>
+          </div>
+        </div>
+
+        {/* Hero */}
+        <div className="container-hero flex flex-col items-center justify-center">
+          <h2 className="text-2xl mb-4">Malerbetrieb Kühnle</h2>
+          <h1 className="text-6xl mb-8 font-bold">
+            Farbe schafft{" "}
+            <span className="bg-gradient-to-r from-blue-500 to-green-500 text-transparent bg-clip-text">
+              Atmosphäre
+            </span>
+            .
+          </h1>
+          <Button variant="default">Kontakt</Button>
+        </div>
+
+        {/* About us */}
+        <div className="container-about px-8 py-8">
+          <div className="about-section flex items-center justify-between">
+            <div className="w-1/2 pr-8">
+              <h1 className="text-4xl mb-4">Über uns.</h1>
+              <p className="text-gray-600">
+                Norbert Kühnle gründete vor 25 Jahren seinen Malerbetrieb mit
+                Sitz in Bad Aibling. Als Meisterbetrieb im Maler- und
+                Lackiererhandwerk stehen Qualitätsarbeit, Zuverlässigkeit und
+                termingerechte Abwicklung der Aufträge im Vordergrund. Wir
+                stehen für fachmännische Handwerksarbeit komplexer Arbeiten rund
+                um die Innen- und Außengestaltung von Räumen, Gebäuden und
+                Fassaden – im Großen wie im Kleinen.
+                <br />
+                <br />
+                Regelmäßige Fortbildungen und der Einsatz neuester Materialien
+                und Arbeitstechniken sichern bis heute die hohe Qualität und
+                Modernität der Leistungen. Von der ersten Beratung über die
+                Planung und Materialauswahl bis hin zur Fertigstellung, sind wir
+                gerne für Sie da.
+              </p>
+            </div>
+            <div className="w-1/2 ">
+              <div className="px-16 py-8 max-w-sm">
+                <img src={about_us} alt="25 Jahre Malerbetrieb Kühnle"></img>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Services */}
+        <div className="container-services">
+          <h1 className="text-4xl mb-4">Services.</h1>
+          <Tabs defaultValue="innenbereich" className="w-[800px]">
+            <TabsList>
+              <TabsTrigger value="innenbereich">Innenbereich</TabsTrigger>
+              <TabsTrigger value="aussenbereich">Außenbereich</TabsTrigger>
+            </TabsList>
+            <TabsContent value="innenbereich">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2">
+                {services_innenbereich.map((number) => (
+                  <Button variant="outline" key={number}>
+                    {number}
+                  </Button>
+                ))}
+              </div>
+            </TabsContent>
+            <TabsContent value="aussenbereich">
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2">
+                {services_aussenbereich.map((number) => (
+                  <Button variant="outline" key={number}>
+                    {number}
+                  </Button>
+                ))}
+              </div>
+            </TabsContent>
+          </Tabs>
+        </div>
+
+        {/* Gallery */}
+
+        <div className="container-gallery p-20">
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+          >
+            <CarouselContent>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <img src={fassade} alt="Fassade" />
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <img src={fassade} alt="Fassade" />
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <img src={fassade} alt="Fassade" />
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <img src={fassade} alt="Fassade" />
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <img src={fassade} alt="Fassade" />
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <img src={fassade} alt="Fassade" />
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+
+        <div className="container-faq">
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
-              <AccordionTrigger>Frage 1</AccordionTrigger>
+              <AccordionTrigger>
+                Machen Sie auch Aussenarbeiten?
+              </AccordionTrigger>
               <AccordionContent>
                 Yes. It adheres to the WAI-ARIA design pattern.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
-              <AccordionTrigger>Frage 2</AccordionTrigger>
+              <AccordionTrigger>
+                Wie lange ist die Vorlaufzeit?
+              </AccordionTrigger>
               <AccordionContent>
                 Yes. It adheres to the WAI-ARIA design pattern.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
-              <AccordionTrigger>Frage 3</AccordionTrigger>
+              <AccordionTrigger>
+                Haben Sie auch Restposten im Angebot?
+              </AccordionTrigger>
               <AccordionContent>
                 Yes. It adheres to the WAI-ARIA design pattern.
               </AccordionContent>
             </AccordionItem>
           </Accordion>
-          <p>
-            Hallo noch ein <b>Text der Fett ist</b>.
-          </p>
         </div>
-      </main>
-      <footer>Das ist der Footer.</footer>
+
+        <div className="container-footer">
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,97 @@
+import instagram from "../assets/instagram.svg";
+import facebook from "../assets/facebook.svg";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Malerbetrieb",
+    links: [
+      {
+        name: "Service",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "Referenzen",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Kontakt",
+        link: "https://www.hoobank.com/create/",
+      },
+    ],
+  },
+  {
+    title: "Rechtliches",
+    links: [
+      {
+        name: "Impressum",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Datenschutz",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Cookie-Einstellungen",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+
+];
+
+export const services_innenbereich = [
+  "Innenanstrich",
+  "Individuelle Farbberatung",
+  "Tapezierarbeiten",
+  "Bodenbeschichtungen",
+  "Spachteltechniken",
+  "Beton Optik",
+  "Lackierarbeiten",
+  "Renovierungsarbeiten",
+  "Verputzarbeiten",
+];
+
+export const services_aussenbereich = [
+  "Holzanstriche aller Art",
+  "Fassadenanstriche",
+  "Individuelle Farbberatung",
+  "Eigener Gerüstbau",
+  "Hochdruck-Wasser-Reinigung",
+  "Lackierarbeiten",
+  "Putzausbesserungen",
+  "Renovierungsarbeiten",
+  "Renova-Fenster Flügel",
+];
+
