bf7df0fbff8b0715e13da3492381d2ec64f74150
Manuel Kühnle <ge36sej@mytum.de>
2024-03-31 22:06:10 +0200

    update readme

--- README.md
+++ README.md
@@ -1,31 +1,16 @@
-# React + TypeScript + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Malerbetrieb Kühnle
 
-Currently, two official plugins are available:
+Website für Malerbetrieb Kühnle aus Bad Aibling.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Autor
 
-## Expanding the ESLint configuration
+- [@m-kuehnle](https://www.github.com/m-kuehnle)
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
 
-- Configure the top-level `parserOptions` property like this:
+## Tech Stack
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
+**Client:** Vite, React, Typescript, TailwindCSS
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
-# malerbetrieb-react
+**Server:** Node
+
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,67 +5,71 @@ import { footerLinks, socialMedia } from "../../constants/index.ts";
 import "../../App.css";
 
 const Footer = () => (
-  <section className={`${styles.flexCenter} ${styles.paddingY} container-footer flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-      <div className="flex-[1] flex flex-col justify-start mr-10">
-        <img
-          src={logo}
-          alt="Malerbetrieb Kühnle"
-          className="w-[120px] h-auto object-contain"
-        />
-        <p className={`${styles.paragraph} mt-4 max-w-[312px] text-gray-500`}>
-          Wir bringen Farbe in Ihr Leben.
-        </p>
-      </div>
+  <section
+    className={`${styles.flexCenter} ${styles.paddingY} container-footer flex-col sm:mt-16 mt-6`}
+  >
+    <div>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className="flex-[1] flex flex-col justify-start mr-10">
+          <img
+            src={logo}
+            alt="Malerbetrieb Kühnle"
+            className="w-[120px] h-auto object-contain"
+          />
+          <p className={`${styles.paragraph} mt-4 max-w-[312px] text-gray-500`}>
+            Wir bringen Farbe in Ihr Leben.
+          </p>
+        </div>
 
-      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
-        {footerLinks.map((footerlink) => (
-          <div
-            key={footerlink.title}
-            className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}
-          >
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-gray-900">
-              {footerlink.title}
-            </h4>
-            <ul className="list-none mt-4">
-              {footerlink.links.map((link, index) => (
-                <li
-                  key={link.title}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-gray-500 hover:text-[#027f3f] cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                  }`}
-                  onClick={() => (window.location.href = link.path)} // Change here
-                >
-                  {link.title}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    </div>
-    <Separator />
-
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6">
-      <p className="font-poppins font-normal text-center text-[14px] leading-[27px] text-gray-900">
-        Copyright Ⓒ 2024 Malerbetrieb Kühnle.
-      </p>
-
-      <div className="flex flex-row md:mt-0 mt-6">
-        {socialMedia.map((social, index) => {
-          const Icon = social.icon;
-          return (
+        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+          {footerLinks.map((footerlink) => (
             <div
-              key={social.id}
-              className={`w-6 h-6 cursor-pointer hover:text-[#027f3f] duration-300 text-gray-900 ${
-                index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-              }`}
-              onClick={() => (window.location.href = social.path)} // Change here
+              key={footerlink.title}
+              className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}
             >
-              <Icon />
+              <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-gray-900">
+                {footerlink.title}
+              </h4>
+              <ul className="list-none mt-4">
+                {footerlink.links.map((link, index) => (
+                  <li
+                    key={link.title}
+                    className={`font-poppins font-normal text-[16px] leading-[24px] text-gray-500 hover:text-[#027f3f] cursor-pointer ${
+                      index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                    }`}
+                    onClick={() => (window.location.href = link.path)} // Change here
+                  >
+                    {link.title}
+                  </li>
+                ))}
+              </ul>
             </div>
-          );
-        })}
+          ))}
+        </div>
+      </div>
+      <Separator />
+
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6">
+        <p className="font-poppins font-normal text-center text-[14px] leading-[27px] text-gray-900">
+          Copyright Ⓒ 2024 Malerbetrieb Kühnle.
+        </p>
+
+        <div className="flex flex-row md:mt-0 mt-6">
+          {socialMedia.map((social, index) => {
+            const Icon = social.icon;
+            return (
+              <div
+                key={social.id}
+                className={`w-6 h-6 cursor-pointer hover:text-[#027f3f] duration-300 text-gray-900 ${
+                  index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+                }`}
+                onClick={() => (window.location.href = social.path)} // Change here
+              >
+                <Icon />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   </section>
