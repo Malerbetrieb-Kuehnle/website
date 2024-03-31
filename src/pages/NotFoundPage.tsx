90c54469647baa7da4af16333e71f9a5481c6afb
Manuel Kühnle <ge36sej@mytum.de>
2024-03-31 22:20:05 +0200

    update text and style of 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,19 +1,20 @@
-import { Link } from 'react-router-dom';
-import { FaExclamationTriangle } from 'react-icons/fa';
+import { FaExclamationTriangle } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <section className='text-center flex flex-col justify-center items-center h-96'>
-      <FaExclamationTriangle className='text-yellow-400 text-6xl mb-4' />
-      <h1 className='text-6xl font-bold mb-4'>404 Not Found</h1>
-      <p className='text-xl mb-5'>This page does not exist</p>
-      <Link
-        to='/'
-        className='text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4'
-      >
-        Go Back
+    <section className="text-center flex flex-col justify-center items-center h-96">
+      <FaExclamationTriangle className="text-[#377d46] text-6xl mb-4" />
+      <h1 className="text-6xl font-bold mb-4">Fehler 404</h1>
+      <p className="text-xl mb-5">
+        Seite nicht gefunden. Diese Seite existiert nicht.
+      </p>
+      <Link to="/">
+        <Button variant="outline">Zurück zur Homepage</Button>
       </Link>
     </section>
   );
 };
+
 export default NotFoundPage;
