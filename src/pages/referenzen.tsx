f2d2aaf7d3a9879938d1ef923c86ba78f5bd889b
Manuel Kühnle <ge36sej@mytum.de>
2024-04-02 22:28:59 +0200

    add reference images

--- src/pages/referenzen.tsx
+++ src/pages/referenzen.tsx
@@ -1,34 +1,178 @@
 import { ParallaxScroll } from "@/components/ui/parallax-scroll";
 
 // Import the images
-import Wohnhaus_Hohenthann from "../assets/Wohnhaus_Hohenthann.jpg";
-import Wohnhaus_Bad_Aibling from "../assets/Wohnhaus _Bad_Aibling.jpg";
-import Bauernhof_Brannenburg from "../assets/Bauernhof_Brannenburg.jpg";
-import Bauernhof_Oberpframmern from "../assets/Bauernhof_Oberpframmern.jpg";
+import Alu_Fensterabdeckung from "../assets/referenzen/Alu_Fensterabdeckung.jpg";
+import Amtsgericht_Bad_Aibling from "../assets/referenzen/Amtsgericht_Bad_Aibling.jpg";
+import Augencentrum_Rosenheim1 from "../assets/referenzen/Augencentrum_Rosenheim1.jpg";
+import Augencentrum_Rosenheim2 from "../assets/referenzen/Augencentrum_Rosenheim2.jpg";
+import Augencentrum_Rosenheim3 from "../assets/referenzen/Augencentrum_Rosenheim3.jpg";
+import Augencentrum_Rosenheim4 from "../assets/referenzen/Augencentrum_Rosenheim4.jpg";
+import Autohalle_Kolbermoor from "../assets/referenzen/Autohalle_Kolbermoor.jpg";
+import Baderhaus_Bad_Aibling1 from "../assets/referenzen/Baderhaus_Bad_Aibling1.jpg";
+import Baderhaus_Bad_Aibling2 from "../assets/referenzen/Baderhaus_Bad_Aibling2.jpg";
+import Bauernhaus_Brannenburg1 from "../assets/referenzen/Bauernhaus_Brannenburg1.jpg";
+import Bauernhof_Brannenburg2 from "../assets/referenzen/Bauernhof_Brannenburg2.jpg";
+import Bauernhof_Oberpframmern1 from "../assets/referenzen/Bauernhof_Oberpframmern1.jpg";
+import Bauernhof_Oberpframmern3 from "../assets/referenzen/Bauernhof_Oberpframmern3.jpg";
+import Buerogebaeude_Bad_Aibling1 from "../assets/referenzen/Wohnhaus_Bruckmühl1.jpg";
+import Buerogebaeude_Bad_Aibling2 from "../assets/referenzen/Wohnhaus_Bruckmühl2.jpg";
+import Buerogebaeude_Bad_Aibling4 from "../assets/referenzen/Wohnhaus_Bruckmühl4.jpg";
+import Buerogebaeude_Bad_Aibling5 from "../assets/referenzen/Wohnhaus_Bruckmühl5.jpg";
+import Fassade_Bad_Aibling from "../assets/referenzen/Fassade_Bad_Aibling.jpg";
+import Fassade_Grabenstaett from "../assets/referenzen/Fassade_Grabenstätt.jpg";
+import Forschungshaus_Bad_Aibling from "../assets/referenzen/Forschungshaus_Bad_Aibling.jpg";
+import Garage_Bad_Aibling from "../assets/referenzen/Garage_Bad_Aibling.jpg";
+import Garage_Bruckmuehl from "../assets/referenzen/Garage_Bruckmühl.jpg";
+import Geschaeftshaus_Rosenheim from "../assets/referenzen/Geschäftshaus_Rosenheim .jpg";
+import Haus from "../assets/referenzen/Haus.jpg";
+import Klinik_St_Georg_Bad_Aiblung from "../assets/referenzen/Klinik_St._Georg_Bad_Aiblung.jpg";
+import Lackarbeiten1 from "../assets/referenzen/Lackarbeiten1.jpg";
+import Lackarbeiten2 from "../assets/referenzen/Lackarbeiten2.jpg";
+import Metzgerei_Rumpel_Bruckmuehl from "../assets/referenzen/Metzgerei_Rumpel_Bruckmühl.jpg";
+import Praxis_Schoenau1 from "../assets/referenzen/Praxis_Schönau1.jpg";
+import Praxis_Schoenau2 from "../assets/referenzen/Praxis_Schönau2.jpg";
+import Praxis_Schoenau3 from "../assets/referenzen/Praxis_Schönau3.jpg";
+import Schloss_Maxhofen_Bruckmuehl from "../assets/referenzen/Schloss_Maxhofen_Bruckmühl.jpg";
+import Schreinerei_Vagen1 from "../assets/referenzen/Schreinerei_Vagen1.jpg";
+import Schreinerei_Vagen2 from "../assets/referenzen/Schreinerei_Vagen2.jpg";
+import Sparkasse_Vagen from "../assets/referenzen/Sparkasse_Vagen.jpg";
+import Sparkassenhochhaus_Rosenheim from "../assets/referenzen/Sparkassenhochhaus_Rosenheim .jpg";
+import Wintergarten_Hohenthann1 from "../assets/referenzen/Wintergarten_Hohenthann1.jpg";
+import Wintergarten_Hohenthann2 from "../assets/referenzen/Wintergarten_Hohenthann2.jpg";
+import Wintergarten_Hohenthann3 from "../assets/referenzen/Wintergarten_Hohenthann3.jpg";
+import Wohnhaus_Bad_Aibling1 from "../assets/referenzen/Wohnhaus_Bad_Aibling1.jpg";
+import Wohnhaus_Bad_Aibling2 from "../assets/referenzen/Wohnhaus_Bad_Aibling2.jpg";
+import Wohnhaus_Bad_Aibling3 from "../assets/referenzen/Wohnhaus_Bad_Aibling3.jpg";
+import Wohnhaus_Bad_Aibling5 from "../assets/referenzen/Wohnhaus_Bad_Aibling5.jpg";
+import Wohnhaus_Bad_Aibling6 from "../assets/referenzen/Wohnhaus_Bad_Aibling6.jpg";
+import Wohnhaus_Bad_Aibling7 from "../assets/referenzen/Wohnhaus_Bad_Aibling7.jpg";
+import Wohnhaus_Bad_Aibling9 from "../assets/referenzen/Wohnhaus_Bad_Aibling9.jpg";
+import Wohnhaus_Bad_Aibling10 from "../assets/referenzen/Wohnhaus_Bad_Aibling10.jpg";
+import Wohnhaus_Bad_Feilnbach1 from "../assets/referenzen/Wohnhaus_Bad_Feilnbach1.jpg";
+import Wohnhaus_Bad_Feilnbach2 from "../assets/referenzen/Wohnhaus_Bad_Feilnbach2.jpg";
+import Wohnhaus_Bad_Feilnbach3 from "../assets/referenzen/Wohnhaus_Bad_Feilnbach3.jpg";
+import Wohnhaus_Bad_Feilnbach4 from "../assets/referenzen/Wohnhaus_Bad_Feilnbach4.jpg";
+import Wohnhaus_Bad_Feilnbach5 from "../assets/referenzen/Wohnhaus_Bad_Feilnbach5.jpg";
+import Wohnhaus_Bad_Feilnbach6 from "../assets/referenzen/Wohnhaus_Bad_Feilnbach6.jpg";
+import Wohnhaus_Beyharting from "../assets/referenzen/Wohnhaus_Beyharting.jpg";
+import Wohnhaus_Bruckmuehl1 from "../assets/referenzen/Wohnhaus_Bruckmühl1.jpg";
+import Wohnhaus_Bruckmuehl2 from "../assets/referenzen/Wohnhaus_Bruckmühl2.jpg";
+import Wohnhaus_Bruckmuehl3 from "../assets/referenzen/Wohnhaus_Bruckmühl3.jpg";
+import Wohnhaus_Bruckmuehl4 from "../assets/referenzen/Wohnhaus_Bruckmühl4.jpg";
+import Wohnhaus_Bruckmuehl5 from "../assets/referenzen/Wohnhaus_Bruckmühl5.jpg";
+import Wohnhaus_Feldkirchen1 from "../assets/referenzen/Wohnhaus_Feldkirchen1.jpg";
+import Wohnhaus_Feldkirchen2 from "../assets/referenzen/Wohnhaus_Feldkirchen2.jpg";
+import Wohnhaus_Feldkirchen3 from "../assets/referenzen/Wohnhaus_Feldkirchen3.jpg";
+import Wohnhaus_Glonn from "../assets/referenzen/Wohnhaus_Glonn.jpg";
+import Wohnhaus_Grabenstaett3 from "../assets/referenzen/Wohnhaus_Grabenstätt3.jpg";
+import Wohnhaus_Grabenstaett4 from "../assets/referenzen/Wohnhaus_Grabenstätt4.jpg";
+import Wohnhaus_Grafing1 from "../assets/referenzen/Wohnhaus_Grafing1.jpg";
+import Wohnhaus_Grafing2 from "../assets/referenzen/Wohnhaus_Grafing2.jpg";
+import Wohnhaus_Hoehenrain from "../assets/referenzen/Wohnhaus_Höhenrain .jpg";
+import Wohnhaus_Hohenthann1 from "../assets/referenzen/Wohnhaus_Hohenthann1.jpg";
+import Wohnhaus_Hohenthann2 from "../assets/referenzen/Wohnhaus_Hohenthann2.jpg";
+import Wohnhaus_Hohenthann3 from "../assets/referenzen/Wohnhaus_Hohenthann3.jpg";
+import Wohnhaus_Hohenthann4 from "../assets/referenzen/Wohnhaus_Hohenthann4.jpg";
+import Wohnhaus_Hohenthann5 from "../assets/referenzen/Wohnhaus_Hohenthann5.jpg";
+import Wohnhaus_Hohenthann6 from "../assets/referenzen/Wohnhaus_Hohenthann6.jpg";
+import Wohnhaus_Kolbermoor from "../assets/referenzen/Wohnhaus_Kolbermoor.jpg";
+import Wohnhaus_Neubeuern from "../assets/referenzen/Wohnhaus_Neubeuern .jpg";
+import Wohnhaus_Pang from "../assets/referenzen/Wohnhaus_Pang .jpg";
+import Wohnhaus_Rosenheim from "../assets/referenzen/Wohnhaus_Rosenheim.jpg";
+import Wohnhaus_Tuntenhausen1 from "../assets/referenzen/Wohnhaus_Tuntenhausen1.jpg";
 
 const images = [
-    Wohnhaus_Hohenthann,
-    Wohnhaus_Bad_Aibling,
-    Bauernhof_Brannenburg,
-    Bauernhof_Oberpframmern,
-    Wohnhaus_Hohenthann,
-    Wohnhaus_Bad_Aibling,
-    Bauernhof_Brannenburg,
-    Bauernhof_Oberpframmern,
-    Wohnhaus_Hohenthann,
-    Wohnhaus_Bad_Aibling,
-    Bauernhof_Brannenburg,
-    Bauernhof_Oberpframmern,
-  ];
+  Alu_Fensterabdeckung,
+  Amtsgericht_Bad_Aibling,
+  Augencentrum_Rosenheim1,
+  Augencentrum_Rosenheim2,
+  Augencentrum_Rosenheim3,
+  Augencentrum_Rosenheim4,
+  Autohalle_Kolbermoor,
+  Baderhaus_Bad_Aibling1,
+  Baderhaus_Bad_Aibling2,
+  Bauernhaus_Brannenburg1,
+  Bauernhof_Brannenburg2,
+  Bauernhof_Oberpframmern1,
+  Bauernhof_Oberpframmern3,
+  Buerogebaeude_Bad_Aibling1,
+  Buerogebaeude_Bad_Aibling2,
+  Buerogebaeude_Bad_Aibling4,
+  Buerogebaeude_Bad_Aibling5,
+  Fassade_Bad_Aibling,
+  Fassade_Grabenstaett,
+  Forschungshaus_Bad_Aibling,
+  Garage_Bad_Aibling,
+  Garage_Bruckmuehl,
+  Geschaeftshaus_Rosenheim,
+  Haus,
+  Klinik_St_Georg_Bad_Aiblung,
+  Lackarbeiten1,
+  Lackarbeiten2,
+  Metzgerei_Rumpel_Bruckmuehl,
+  Praxis_Schoenau1,
+  Praxis_Schoenau2,
+  Praxis_Schoenau3,
+  Schloss_Maxhofen_Bruckmuehl,
+  Schreinerei_Vagen1,
+  Schreinerei_Vagen2,
+  Sparkasse_Vagen,
+  Sparkassenhochhaus_Rosenheim,
+  Wintergarten_Hohenthann1,
+  Wintergarten_Hohenthann2,
+  Wintergarten_Hohenthann3,
+  Wohnhaus_Bad_Aibling1,
+  Wohnhaus_Bad_Aibling2,
+  Wohnhaus_Bad_Aibling3,
+  Wohnhaus_Bad_Aibling5,
+  Wohnhaus_Bad_Aibling6,
+  Wohnhaus_Bad_Aibling7,
+  Wohnhaus_Bad_Aibling9,
+  Wohnhaus_Bad_Aibling10,
+  Wohnhaus_Bad_Feilnbach1,
+  Wohnhaus_Bad_Feilnbach2,
+  Wohnhaus_Bad_Feilnbach3,
+  Wohnhaus_Bad_Feilnbach4,
+  Wohnhaus_Bad_Feilnbach5,
+  Wohnhaus_Bad_Feilnbach6,
+  Wohnhaus_Beyharting,
+  Wohnhaus_Bruckmuehl1,
+  Wohnhaus_Bruckmuehl2,
+  Wohnhaus_Bruckmuehl3,
+  Wohnhaus_Bruckmuehl4,
+  Wohnhaus_Bruckmuehl5,
+  Wohnhaus_Feldkirchen1,
+  Wohnhaus_Feldkirchen2,
+  Wohnhaus_Feldkirchen3,
+  Wohnhaus_Glonn,
+  Wohnhaus_Grabenstaett3,
+  Wohnhaus_Grabenstaett4,
+  Wohnhaus_Grafing1,
+  Wohnhaus_Grafing2,
+  Wohnhaus_Hoehenrain,
+  Wohnhaus_Hohenthann1,
+  Wohnhaus_Hohenthann2,
+  Wohnhaus_Hohenthann3,
+  Wohnhaus_Hohenthann4,
+  Wohnhaus_Hohenthann5,
+  Wohnhaus_Hohenthann6,
+  Wohnhaus_Kolbermoor,
+  Wohnhaus_Neubeuern,
+  Wohnhaus_Pang,
+  Wohnhaus_Rosenheim,
+  Wohnhaus_Tuntenhausen1,
+];
 
-export default function Referenzen(){
-    return (<div>
-         {/* Referenzen */}
-         <div className="container-standard flex flex-col">
-          <h1 className="text-6xl mb-6 mt-6 font-bold text-center">
-            Referenzen.
-          </h1>
-          <ParallaxScroll images={images} />
-        </div>
-    </div>)
+export default function referenzen() {
+  return (
+    <div>
+      {/* referenzen */}
+      <div className="container-standard flex flex-col">
+        <h1 className="text-6xl mb-6 mt-6 font-bold text-center">
+          Referenzen.
+        </h1>
+        <ParallaxScroll images={images} />
+      </div>
+    </div>
+  );
 }
