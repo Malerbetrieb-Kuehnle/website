4874b3a3ae7177402c6e375979e9aa66827fa3c9
Manuel Kühnle <ge36sej@mytum.de>
2024-04-03 19:12:04 +0200

    add text for impressum and datenschutz

--- src/pages/impressum.tsx
+++ src/pages/impressum.tsx
@@ -1,3 +1,75 @@
 export default function Impressum() {
-  return <h1>Impressum</h1>;
+  const p = "mt-6 mb-10 text-lg leading-8 text-gray-500";
+  const h1 = "mt-2 mb-8 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl";
+  const h2 = "mt-2 text-3xl  tracking-tight text-gray-900 sm:text-4xl";
+
+  return (
+    <>
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl">
+            <div className="max-w-7xl mx-auto">
+              <h1 className={h1}>
+                Impressum
+              </h1>
+
+              <p className={p}>
+                Norbert K&uuml;hnle
+                <br />
+                Malerbetrieb K&uuml;hnle
+                <br />
+                Ebersberger Stra&szlig;e 75
+                <br />
+                83043 Bad Aibling
+              </p>
+
+              <h2 className={h2}>
+                Kontakt
+              </h2>
+              <p className={p}>
+                Telefon: +49 8061 93 98 333
+                <br />
+                E-Mail: malerbetrieb.kuehnle@gmx.de
+              </p>
+
+              <h2 className={h2}>
+                Umsatzsteuer-ID
+              </h2>
+              <p className={p}>
+                Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a
+                Umsatzsteuergesetz:
+                <br />
+                DE204642150
+              </p>
+
+              <h2 className={h2}>
+                EU-Streitschlichtung
+              </h2>
+              <p className={p}>
+                Die Europ&auml;ische Kommission stellt eine Plattform zur
+                Online-Streitbeilegung (OS) bereit:{" "}
+                <a
+                  href="https://ec.europa.eu/consumers/odr/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  https://ec.europa.eu/consumers/odr/
+                </a>
+                .<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.
+              </p>
+
+              <h2 className={h2}>
+                Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+              </h2>
+              <p className={p}>
+                Wir sind nicht bereit oder verpflichtet, an
+                Streitbeilegungsverfahren vor einer
+                Verbraucherschlichtungsstelle teilzunehmen.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
