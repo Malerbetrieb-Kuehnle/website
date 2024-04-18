053205208b5d8351e0e371418eae3582477a8451
Manuel Kühnle <ge36sej@mytum.de>
2024-04-18 18:36:24 +0200

    fix faq import

--- src/seiten/HomePage.tsx
+++ src/seiten/HomePage.tsx
@@ -1,11 +1,11 @@
+import Hero from "@/components/hero";
 import Contact from "@/components/contact";
 import References from "@/components/references";
 import Malerbetrieb from "@/components/malerbetrieb";
 import Services from "@/components/services";
 import Partner from "@/components/partner";
 import Ausbildung from "@/components/ausbildung";
-import Faq from "@/components/faq";
-import Hero from "@/components/hero";
+import Faq from "@/components/fragen";
 
 const HomePage = () => {
   return (
